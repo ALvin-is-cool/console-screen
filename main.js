9e15c9c3d14e78fcb4bead837279127e5d4f8ad5
ALvin-is-cool <86335688+ALvin-is-cool@users.noreply.github.com>
2021-10-16 14:58:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("SUp all the people of the earth");
+var first_name="The cool boy alvin";
+var Second_name=" and The cool man dad";
+console.log(first_name+Second_name);
+var A =3;
+var B =12;
+var multiply =A*B;
+console.log (multiply);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
